f6326bca14f0b035a6e7dcc80a19cd8f86e58cec
juantoman <juan.torres@iestacio.com>
2016-04-25 12:28:47 +0200

    Formularis

--- leaderboard.js
+++ leaderboard.js
@@ -30,11 +30,26 @@ if(Meteor.isClient){
 		'click .decrement': function(){
 			var selectedPlayer = Session.get('selectedPlayer');
 			PlayersList.update({ _id: selectedPlayer }, {$inc: {score: -5} });
-}
-
+		},
+		'click .remove': function(){
+			var selectedPlayer = Session.get('selectedPlayer');
+    			PlayersList.remove({ _id: selectedPlayer });
+		}
 
 	});
-
+	Template.addPlayerForm.events({
+		'submit form': function(event){
+			event.preventDefault();
+			var playerNameVar = event.target.playerName.value;
+			if( playerNameVar ){
+				PlayersList.insert({
+					name: playerNameVar,
+					score: 0
+				});
+				event.target.playerName.value = "";
+			}
+		}
+	});
 }
 
 if(Meteor.isServer){
