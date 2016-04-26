28561672674251a114548a24fa0a754e910cdd4a
juantoman <juan.torres@iestacio.com>
2016-04-26 11:08:04 +0200

    Methods

--- leaderboard.js
+++ leaderboard.js
@@ -26,32 +26,24 @@ if(Meteor.isClient){
     			Session.set('selectedPlayer', playerId);
 		},
 		'click .increment': function(){
-			var selectedPlayer = Session.get('selectedPlayer');
-			PlayersList.update({ _id: selectedPlayer }, { $inc: {score: 5 }});
+		  var selectedPlayer = Session.get('selectedPlayer');
+		  Meteor.call('updateScore', selectedPlayer, 5);
 		},
 		'click .decrement': function(){
-			var selectedPlayer = Session.get('selectedPlayer');
-			PlayersList.update({ _id: selectedPlayer }, {$inc: {score: -5} });
+		    var selectedPlayer = Session.get('selectedPlayer');
+		    Meteor.call('updateScore', selectedPlayer, -5);
 		},
 		'click .remove': function(){
-			var selectedPlayer = Session.get('selectedPlayer');
-    			PlayersList.remove({ _id: selectedPlayer });
+		  var selectedPlayer = Session.get('selectedPlayer');
+		  Meteor.call('removePlayer', selectedPlayer);
 		}
-
 	});
 	Template.addPlayerForm.events({
 		'submit form': function(event){
-			event.preventDefault();
-			var playerNameVar = event.target.playerName.value;
-			var currentUserId = Meteor.userId();
-			if( playerNameVar ){
-				PlayersList.insert({
-					name: playerNameVar,
-					score: 0,
-					createdBy: currentUserId
-				});
-				event.target.playerName.value = "";
-			}
+		  event.preventDefault();
+		  var playerNameVar = event.target.playerName.value;
+		  Meteor.call('createPlayer', playerNameVar);
+		  event.target.playerName.value = "";
 		}
 	});
 }
@@ -62,3 +54,33 @@ if(Meteor.isServer){
     return PlayersList.find({ createdBy: currentUserId });
 	});
 }
+
+Meteor.methods({
+    'createPlayer': function(playerNameVar){
+        check(playerNameVar, String);
+        var currentUserId = Meteor.userId();
+        if(currentUserId){
+            PlayersList.insert({
+                name: playerNameVar,
+                score: 0,
+                createdBy: currentUserId
+            });
+        }
+    },
+		'removePlayer': function(selectedPlayer){
+		    check(selectedPlayer, String);
+		    var currentUserId = Meteor.userId();
+		    if(currentUserId){
+		        PlayersList.remove({ _id: selectedPlayer, createdBy: currentUserId });
+		    }
+		},
+		'updateScore': function(selectedPlayer, scoreValue){
+	    check(selectedPlayer, String);
+	    check(scoreValue, Number);
+	    var currentUserId = Meteor.userId();
+	    if(currentUserId){
+	        PlayersList.update( { _id: selectedPlayer, createdBy: currentUserId },
+	                            { $inc: {score: scoreValue} });
+	    }
+		}
+});
